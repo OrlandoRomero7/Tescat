14d21b037ea16871a2e7b51cc2fda067d69d46dd
OrlandoRomero7 <al18760462@ite.edu.mx>
2023-09-11 14:13:09 -0700

    se muestra las credenciales de usuario en UserDetailsPage sin errores

--- Services/UserCredential/UserCredentialService.cs
+++ Services/UserCredential/UserCredentialService.cs
@@ -5,7 +5,7 @@ namespace Tescat.Services.UserCredentials
 {
     public class UserCredentialService : IUserCredentialService
     {
-        private readonly IDbContextFactory<TescatDbContext>  _contextFactory;
+        private readonly IDbContextFactory<TescatDbContext> _contextFactory;
         public UserCredentialService(IDbContextFactory<TescatDbContext> contextFactory)
         {
             _contextFactory = contextFactory;
@@ -13,10 +13,40 @@ namespace Tescat.Services.UserCredentials
         public async Task<UserCredential> GetUserCredentials(int userId)
         {
             using var context = _contextFactory.CreateDbContext();
-            return await context.UserCredentials.FirstOrDefaultAsync(p => p.IdUser == userId);
+            var userCredentialDb = await context.UserCredentials.FirstOrDefaultAsync(p => p.IdUser == userId);
+            if (userCredentialDb != null)
+            {
+               return userCredentialDb;
+
+            }
+            else
+            {
+                return new UserCredential();
+            }
             //return await _contextFactory.UserCredentialService.FirstOrDefaultAsync(p => p.IdUser == userId);
 
         }
+        public async Task<UserCredential> InsertUserCredentials(UserCredential userCredential)
+        {
+            using var context = _contextFactory.CreateDbContext();
+            context.UserCredentials.Add(userCredential);
+            await context.SaveChangesAsync();
+            return userCredential;
+        }
+
+        public async Task<UserCredential> DeleteUserCredentials(int userID)
+        {
+            using var context = _contextFactory.CreateDbContext();
+            var userCredentialDb = await context.UserCredentials.FindAsync(userID);
+            if (userCredentialDb == null) return null;
+
+            context.UserCredentials.Remove(userCredentialDb);
+            await context.SaveChangesAsync();
+            return userCredentialDb;
+        }       
         
+
+        
+
     }
 }
