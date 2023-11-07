88bceb7797ee2e46929b56240f700d9c68f5ec17
OrlandoRomero7 <al18760462@ite.edu.mx>
2023-11-07 14:30:19 -0800

    monejando listas en storage, se hace crud con listas

--- Services/Storage/StorageService.cs
+++ Services/Storage/StorageService.cs
@@ -22,25 +22,26 @@ namespace Tescat.Services.Storages
             return storage;
         }
 
-        public async Task<List<Storage>> GetStoragesWithoutIdPC()
+        public async Task<List<Storage>> GetListStoragesWithoutIdPC()
         {
             using var context = _contextFactory.CreateDbContext();
             return await context.Storages.Where(s => s.IdPc == null).ToListAsync();
         }
 
-        public async Task<Storage> GetStorageWithPcId(Guid guid)
+        //public async Task<List<Storage>> GetStorageWithoutIdPC()
+        //{
+        //    using var context = _contextFactory.CreateDbContext();
+        //    return await context.Storages.Where(s => s.IdPc == null).ToListAsync();
+        //}
+
+        
+
+        public async Task<List<Storage>> GetStorageWithPcId(Guid guid)
         {
             using var context = _contextFactory.CreateDbContext();
-            var storageDb = await context.Storages
-                                 .FirstOrDefaultAsync(storage => storage.IdPc == guid);
-            if (storageDb != null)
-            {
-                return storageDb;
-            }
-            else
-            {
-                return new Storage();
-            }
+            var storageDbList = await context.Storages
+                                 .Where(storage => storage.IdPc == guid).ToListAsync();
+            return storageDbList;
         }
         public async Task<Storage> GetStorage(Guid guid)
         {
@@ -65,22 +66,45 @@ namespace Tescat.Services.Storages
             return storage;
         }
 
-        public async Task<Storage> UpdateStorage(Storage storage, Guid IdPc)
+        //public async Task<Storage> UpdateStorage(Storage storage, Guid IdPc)
+        //{
+        //    using var context = _contextFactory.CreateDbContext();
+        //    bool existeStorage = context.Storages.Any(c => c.IdPc == IdPc);
+        //    if (existeStorage)
+        //    {
+        //        context.Entry(storage).State = EntityState.Modified;
+        //        await context.SaveChangesAsync();
+        //        return storage;
+        //    }
+        //    else
+        //    {
+        //        storage.IdPc = IdPc;
+        //        return await InsertStorage(storage);
+        //    }
+
+        //}
+        public async Task<List<Storage>> UpdateStorages(List<Storage> updatedStorages, Guid IdPc)
         {
             using var context = _contextFactory.CreateDbContext();
-            bool existeStorage = context.Storages.Any(c => c.IdPc == IdPc);
-            if (existeStorage)
+
+            foreach (var updatedStorage in updatedStorages)
             {
-                context.Entry(storage).State = EntityState.Modified;
-                await context.SaveChangesAsync();
-                return storage;
+                if (updatedStorage.IdStorage != Guid.Empty)
+                {
+  
+                    context.Entry(updatedStorage).State = EntityState.Modified;
+                }
+                else
+                {
+                    updatedStorage.IdPc = IdPc;
+                    context.Storages.Add(updatedStorage);
+                   // context.Entry(updatedStorage).State = EntityState.Added;
+                }
+                
             }
-            else
-            {
-                storage.IdPc = IdPc;
-                return await InsertStorage(storage);
-            }
-            
+            await context.SaveChangesAsync();
+
+            return updatedStorages;
         }
 
         public async Task<Storage> UpdateStorageForStock(Storage storage)
